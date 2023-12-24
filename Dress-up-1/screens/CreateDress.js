d03ade8ea3b02e8be3121d57fb470459e4939801
prachi1505 <85737452+prachi1505@users.noreply.github.com>
2023-12-24 14:48:55 +0530

    Add files via upload

---
+++ Dress-up-1/screens/CreateDress.js
@@ -0,0 +1,195 @@
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  Image,
+  ScrollView,
+  TextInput,
+  Dimensions
+} from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+import DropDownPicker from "react-native-dropdown-picker";
+
+export default class CreateDress extends Component {
+  render() {
+  if(!this.state.fontsLoaded){
+return <AppLoading/>;
+  }
+  else{
+    let preview_image ={
+      "image-1" : require("./assets/image-1.png"),
+      "image-2" : require("./assets/image-2.png"),
+      "image-3" : require("./assets/image-3.jpg")
+    };
+    return (
+      <View style={styles.container}>
+        <SafeAreaView style={styles.droidSafeArea} />
+        <View style={styles.appTitle}>
+          <View style={styles.appIcon}>
+            <Image
+              source={require("../assets/logo.png")}
+              style={styles.iconImage}
+            ></Image>
+          </View>
+          <View style={styles.appTitleTextContainer}>
+            <Text style={styles.appTitleText}>New Dress</Text>
+          </View>
+        </View>
+        <View style={styles.fieldsContainer}>
+   
+    
+        <Image
+              source={preview_images[this.state.previewImage]}
+              style={styles.previewImage}
+            ></Image>
+            <View style={{ height: RFValue(this.state.dropdownHeight) }}>
+              <DropDownPicker
+                items={[
+                  { label: "Image 1", value: "image-1" },
+                  { label: "Image 2", value: "image-2" },
+                  { label: "Image 3", value: "image-3" },
+            
+                ]}
+                defaultValue={this.state.previewImage}
+                open={this.state.dropdownHeight == 170 ? true : false}
+                onOpen={() => {
+                  this.setState({ dropdownHeight: 170 });
+                }}
+                onClose={() => {
+                  this.setState({ dropdownHeight: 40 });
+                }}
+                style={{
+                  backgroundColor: "transparent",
+                  borderWidth: 1,
+                  borderColor: "white",
+                }}
+                textStyle={{
+                  color: this.state.dropdownHeight == 170 ? "black" : "white",
+                  fontFamily: "Bubblegum-Sans",
+                }}
+                onSelectItem={(item) =>
+                  this.setState({
+                    previewImage: item.value,
+                  })
+                }
+              />
+            </View>
+            <ScrollView>
+              <TextInput
+                style={styles.inputFont}
+                onChangeText={(title) => this.setState({ title })}
+                placeholder={"Title"}
+                placeholderTextColor="white"
+              />
+
+              <TextInput
+                style={[
+                  styles.inputFont,
+                  styles.inputFontExtra,
+                  styles.inputTextBig,
+                ]}
+                onChangeText={(description) => this.setState({ description })}
+                placeholder={"Description"}
+                multiline={true}
+                numberOfLines={4}
+                placeholderTextColor="white"
+              />
+              <TextInput
+                style={[
+                  styles.inputFont,
+                  styles.inputFontExtra,
+                  styles.inputTextBig,
+                ]}
+                onChangeText={(dress) => this.setState({ dress})}
+                placeholder={"Dress"}
+                multiline={true}
+                numberOfLines={20}
+                placeholderTextColor="white"
+              />
+
+              <TextInput
+                style={[
+                  styles.inputFont,
+                  styles.inputFontExtra,
+                  styles.inputTextBig,
+                ]}
+                onChangeText={(moral) => this.setState({ quotes })}
+                placeholder={"Quotes for the dress"}
+                multiline={true}
+                numberOfLines={4}
+                placeholderTextColor="white"
+              />
+            </ScrollView>
+          </View>
+          <View style={{ flex: 0.08 }} />
+        </View>
+      );
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#15193c",
+  },
+  droidSafeArea: {
+    marginTop:
+      Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35),
+  },
+  appTitle: {
+    flex: 0.07,
+    flexDirection: "row",
+  },
+  appIcon: {
+    flex: 0.3,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  iconImage: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain",
+  },
+  appTitleTextContainer: {
+    flex: 0.7,
+    justifyContent: "center",
+  },
+  appTitleText: {
+    color: "white",
+    fontSize: RFValue(28),
+    fontFamily: "Bubblegum-Sans",
+  },
+  fieldsContainer: {
+    flex: 0.85,
+  },
+  previewImage: {
+    width: "93%",
+    height: RFValue(250),
+    alignSelf: "center",
+    borderRadius: RFValue(10),
+    marginVertical: RFValue(10),
+    resizeMode: "contain",
+  },
+  inputFont: {
+    height: RFValue(40),
+    borderColor: "white",
+    borderWidth: RFValue(1),
+    borderRadius: RFValue(10),
+    paddingLeft: RFValue(10),
+    color: "white",
+    fontFamily: "Bubblegum-Sans",
+  },
+  inputFontExtra: {
+    marginTop: RFValue(15),
+  },
+  inputTextBig: {
+    textAlignVertical: "top",
+    padding: RFValue(5),
+  },
+});
+
---
+++ Dress-up-1/screens/DressScreen.js
@@ -0,0 +1,206 @@
+import React, { Component } from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    SafeAreaView,
+    Platform,
+    StatusBar,
+    Image,
+    ScrollView,
+    Dimensions
+} from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { RFValue } from "react-native-responsive-fontsize";
+
+import * as Font from "expo-font";
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
+
+
+let customFonts = {
+    "Bubblegum-Sans": require("../assets/fonts/BubblegumSans-Regular.ttf")
+};
+
+export default class StoryScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            fontsLoaded: false,
+            speakerColor: "gray",
+            speakerIcon: "volume-high-outline"
+        };
+    }
+
+    async _loadFontsAsync() {
+        await Font.loadAsync(customFonts);
+        this.setState({ fontsLoaded: true });
+    }
+
+    componentDidMount() {
+        this._loadFontsAsync();
+    }
+
+    render() {
+        if (!this.props.route.params) {
+            this.props.navigation.navigate("Home");
+        }else if (this.state.fontsLoaded) {
+            SplashScreen.hideAsync();
+            return (
+                <View style={styles.container}>
+                    <SafeAreaView style={styles.droidSafeArea} />
+                    <View style={styles.appTitle}>
+                        <View style={styles.appIcon}>
+                            <Image
+                                source={require("../assets/logo.png")}
+                                style={styles.iconImage}
+                            ></Image>
+                        </View>
+                        <View style={styles.appTitleTextContainer}>
+                            <Text style={styles.appTitleText}>Dressing App</Text>
+                        </View>
+                    </View>
+                    <View style={styles.dressContainer}>
+                        <ScrollView style={styles.dressCard}>
+                            <Image
+                                source={require("../assets/image-1.png")}
+                                style={styles.image}
+                            ></Image>
+
+                            <View style={styles.dataContainer}>
+                                <View style={styles.titleTextContainer}>
+                                    <Text style={styles.dressTitleText}>
+                                        {this.props.route.params.dress.title}
+                                    </Text>
+                                
+                                    
+                                </View>
+                                <View style={styles.iconContainer}>
+                                    <Ionicons
+                                        name={this.state.speakerIcon}
+                                        size={RFValue(30)}
+                                        color={this.state.speakerColor}
+                                        style={{ margin: RFValue(15) }}
+                                    />
+                                </View>
+                            </View>
+                            <View style={styles.dressTextContainer}>
+                                <Text style={styles.dressText}>
+                                    {this.props.route.params.dress.dress}
+                                </Text>
+                                <Text style={styles.quotesText}>
+                                    Quotes - {this.props.route.params.dress.quotes}
+                                </Text>
+                            </View>
+                            <View style={styles.actionContainer}>
+                                <View style={styles.likeButton}>
+                                    <Ionicons name={"heart"} size={RFValue(30)} color={"white"} />
+                                    <Text style={styles.likeText}>12k</Text>
+                                </View>
+                            </View>
+                        </ScrollView>
+                    </View>
+                </View>
+            );
+        }
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#15193c"
+    },
+    droidSafeArea: {
+        marginTop: Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35)
+    },
+    appTitle: {
+        flex: 0.07,
+        flexDirection: "row"
+    },
+    appIcon: {
+        flex: 0.3,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    iconImage: {
+        width: "100%",
+        height: "100%",
+        resizeMode: "contain"
+    },
+    appTitleTextContainer: {
+        flex: 0.7,
+        justifyContent: "center"
+    },
+    appTitleText: {
+        color: "white",
+        fontSize: RFValue(28),
+        fontFamily: "Bubblegum-Sans"
+    },
+    dressContainer: {
+        flex: 1
+    },
+    dressCard: {
+        margin: RFValue(20),
+        backgroundColor: "#2f345d",
+        borderRadius: RFValue(20)
+    },
+    image: {
+        width: "100%",
+        alignSelf: "center",
+        height: RFValue(200),
+        borderTopLeftRadius: RFValue(20),
+        borderTopRightRadius: RFValue(20),
+        resizeMode: "contain"
+    },
+    dataContainer: {
+        flexDirection: "row",
+        padding: RFValue(20)
+    },
+    titleTextContainer: {
+        flex: 0.8
+    },
+    dressTitleText: {
+        fontFamily: "Bubblegum-Sans",
+        fontSize: RFValue(25),
+        color: "white"
+    },
+
+    iconContainer: {
+        flex: 0.2
+    },
+    dressTextContainer: {
+        padding: RFValue(20)
+    },
+    dressText: {
+        fontFamily: "Bubblegum-Sans",
+        fontSize: RFValue(15),
+        color: "white"
+    },
+    quotesText: {
+        fontFamily: "Bubblegum-Sans",
+        fontSize: RFValue(20),
+        color: "white"
+    },
+    actionContainer: {
+        justifyContent: "center",
+        alignItems: "center",
+        margin: RFValue(10)
+    },
+    likeButton: {
+        width: RFValue(160),
+        height: RFValue(40),
+        flexDirection: "row",
+        backgroundColor: "#eb3948",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: RFValue(30)
+    },
+    likeText: {
+        color: "white",
+        fontFamily: "Bubblegum-Sans",
+        fontSize: RFValue(25),
+        marginLeft: RFValue(5)
+    }
+});
